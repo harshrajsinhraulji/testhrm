93629b3791f2a57c6331a6b2cf73260dff791dd4
Harshrajsinh Raulji <rauljiharsh724@gmail.com>
2026-01-03 10:22:21 +0000

    make the logo minimalistic

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,26 +3,18 @@ import type { SVGProps } from 'react';
 export function DayflowLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      viewBox="0 0 140 28"
-      width="140"
-      height="28"
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <style>{`
-        .dayflow-d { fill: hsl(var(--primary)); }
-        .dayflow-rest { fill: hsl(var(--foreground)); }
-      `}</style>
-      <text
-        x="0"
-        y="24"
-        fontFamily="var(--font-poppins), sans-serif"
-        fontSize="28"
-        fontWeight="700"
-      >
-        <tspan className="dayflow-d">D</tspan>
-        <tspan className="dayflow-rest">ayflow</tspan>
-      </text>
+      <rect width="32" height="32" rx="8" fill="hsl(var(--primary))" />
+      <path
+        d="M12 8V24H18C22.4183 24 26 20.4183 26 16C26 11.5817 22.4183 8 18 8H12ZM16 12H18C20.2091 12 22 13.7909 22 16C22 18.2091 20.2091 20 18 20H16V12Z"
+        fill="white"
+      />
     </svg>
   );
 }
