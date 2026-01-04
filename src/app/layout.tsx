5e77a3fb0b2d42348ab965a18461f12f4abe7353
Harshrajsinh Raulji <rauljiharsh724@gmail.com>
2026-01-04 10:55:27 +0000

    when i hosted the website, it was showing firebase studios icon, change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ const fontPoppins = Poppins({
 export const metadata: Metadata = {
   title: 'Dayflow HR',
   description: 'Streamline Your HR Operations',
+  icons: {
+    icon: `data:image/svg+xml;utf8,<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="32" height="32" rx="8" fill="rgb(110 58 112)" /><path d="M12 8V24H18C22.4183 24 26 20.4183 26 16C26 11.5817 22.4183 8 18 8H12ZM16 12H18C20.2091 12 22 13.7909 22 16C22 18.2091 20.2091 20 18 20H16V12Z" fill="white" /></svg>`
+  },
 };
 
 export default function RootLayout({
